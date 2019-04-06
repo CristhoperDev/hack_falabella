25c05f275783dc3bde9ea341ccc370f7ecf6ec94
unknown <giampieer24@gmail.com>
2019-04-06 14:45:10 -0500

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'detail-scanner/:id', loadChildren: './detail-scanner/detail-scanner.module#DetailScannerPageModule' },
   { path: 'comparison/:id', loadChildren: './comparison/comparison.module#ComparisonPageModule' },
   ];
 @NgModule({
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,6 +3,8 @@ import {RestApiService} from '../rest-api.service';
 import {LoadingController} from '@ionic/angular';
 import {ActivatedRoute, Router} from '@angular/router';
 import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player/ngx';
+import {forEach} from '@angular-devkit/schematics';
+import {first} from 'rxjs/operators';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -29,7 +31,8 @@ export class Tab3Page implements OnInit {
             .subscribe(res => {
                 console.log(res);
                 this.classroom = res;
-            }, err => {
+                console.log(this.classroom.precio);
+                }, err => {
                 console.log(err);
             });
     }
